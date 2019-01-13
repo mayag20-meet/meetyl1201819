9db554972eb4eb758b922efe8b9e4120412667fa
mayag20-meet <mayag20@meet.mit.edu>
2019-01-13 12:16:35 -0500

    agario2

--- mayag_yl1/agario.py
+++ mayag_yl1/agario.py
@@ -22,5 +22,28 @@ max_ball_dy=5
 
 balls=[]
 for i in range(num_balls):
-	
+	x= random.randint(-SCREEN_WIDTH + max_ball_r, SCREEN_WIDTH - max_ball_r)
+	y= random.randint(-SCREEN_HEIGHT + max_ball_r, SCREEN_HEIGHT - max_ball_r)
+	dx= random.randint(min_ball_dx, max_ball_dx)
+	dy= random.randint(min_ball_dy, max_ball_dy)
+	r= random.randint(min_ball_r, max_ball_r)
+	color= (random.random(), random.random(), random.random())
+	balls.append(Ball(x, y, dx, dy, r, color))
+
+for i in (balls[]):
+	i.move(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+def collide(Ball1, Ball2):
+	if ball1 == ball2:
+		return False
+	d= math.sqrt(math.pow((Ball2.x-Ball1.x),2) + math.pow((Ball2.y-Ball1.y),2))
+	if d+10 <= Ball1.r+Ball2.r:
+		return True
+	else:
+		return False
+def all_collide():
+	for Ball1, ball2 in (balls[]):
+
+
+
 
--- mayag_yl1/ball.py
+++ mayag_yl1/ball.py
@@ -13,33 +13,27 @@ class Ball(Turtle):
 		self.shapesize(r/10)
 		self.penup()
 		self.goto(x,y)
+	def move(self, screen_width, screen_height):
+		current_x, current_y= self.pos()
+		new_x= current_x+self.dx
+		new_y= current_y+self.dy
+		right_side_ball=new_x+self.r
+		left_side_ball=new_x-self.r
+		top_side_ball=new_y+self.r
+		bottom_side_ball=new_y-self.r
+		self.goto(new_x, new_y)
+		if right_side_ball>screen_width:
+			self.dx= -self.dx
+		if left_side_ball<-screen_width:
+			self.dx= -self.dx
+		if top_side_ball>screen_height:
+			self.dy= -self.dy
+		if bottom_side_ball<-screen_height:
+			self.dy= -self.dy
 
-		while True:
-		
-		def move(self, screen_width, screen_height):
-			current_x, current_y= self.pos()
-			new_x= current_x+self.dx
-			new_y= current_y+self.dy
-			right_side_ball=new_x+self.r
-			left_side_ball=new_x-self.r
-			top_side_ball=new_y+self.r
-			bottom_side_ball=new_y-self.r
-			self.goto(new_x, new_y)
-			if right_side_ball>screen_width:
-				self.dx= -self.dx
-			if left_side_ball<-screen_width:
-				self.dx= -self.dx
-			if top_side_ball>screen_height:
-				self.dy= -self.dy
-			if bottom_side_ball<-screen_width:
-				self.dy= -self.dy
-
-	
-
-	turtle.tracer
-
-a=Ball(30, 30, 2, 3, 10, "blue")
-a.move(200, 200)
+"""a=Ball(30, 30, 2, 3, 10, "blue")
+while True:
+	a.move(200, 200)"""
 
 turtle.mainloop()
 
